0b59820788a326b99e13ffeb3dbc5ebd4159e5ea
Wavemaker <amciitd2020@gmail.com>
2025-03-09 02:44:35 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className = "App">
-      <header className = "App-header">
-          Hello world!
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            DataIsLoaded: false,
+        };
+    }
+
+    render() {
+        const handleClick = () => {
+            fetch("http://localhost:8080/hello?myName=Aashutosh")
+                .then((res) => res.json())
+                .then((json) => {
+                    this.setState({
+                        items: json,
+                        DataIsLoaded: true,
+                    });
+                });
+        };
+
+        return (
+            <div className = "App">
+                <div className = "Button">
+                    <button onClick={handleClick}>Click Me!</button>
+                    <p>
+                        {JSON.stringify(this.state.items)}
+                    </p>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
